c5de127be1f4a1517df8ed2cd9add5154d49a412
Roberto-ParGon <zs19016407@estudiantes.uv.mx>
2025-04-17 21:54:31 -0600

    [ADD] BottomNavBar Template

--- app/src/main/java/com/uv/taller365/MainActivity.kt
+++ app/src/main/java/com/uv/taller365/MainActivity.kt
@@ -1,20 +1,42 @@
 package com.uv.taller365
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var bottomNav: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        loadFragment(VehiclesFragment())
+        bottomNav = findViewById(R.id.bottomNav)!!
+        bottomNav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.vehiclesNav -> {
+                    loadFragment(VehiclesFragment())
+                    true
+                }
+                R.id.repaisNav -> {
+                    loadFragment(RepairFragment())
+                    true
+                }
+                R.id.clientsNav -> {
+                    loadFragment(ClientsFragment())
+                    true
+                }
+                else -> false
+            }
         }
     }
+
+    private fun loadFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container, fragment)
+        transaction.commit()
+    }
+
 }
