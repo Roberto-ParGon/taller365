7df03eb0bd1fe1dd0db4cdc22a1ae06b30462dae
Roberto-ParGon <zs19016407@estudiantes.uv.mx>
2025-04-19 07:07:57 -0600

    [ADD]Vehicle_Card

--- app/src/main/java/com/uv/taller365/VehiclesFragment.kt
+++ app/src/main/java/com/uv/taller365/VehiclesFragment.kt
@@ -1,61 +1,138 @@
 package com.uv.taller365
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.ListView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [VehiclesFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class VehiclesFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    data class Vehicle(
+        val imageRes: Int,
+        val name: String,
+        val brand: String,
+        val serialNumber: String,
+        val status: String,
+        val statusColor: Int
+    )
+
+    private inner class VehicleAdapter(private val vehicles: List<Vehicle>) :
+        RecyclerView.Adapter<VehicleAdapter.VehicleViewHolder>() {
+
+        inner class VehicleViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+            val ivVehicleImage: ImageView = itemView.findViewById(R.id.ivVehicleImage)
+            val tvVehicleName: TextView = itemView.findViewById(R.id.tvVehicleName)
+            val tvBrand: TextView = itemView.findViewById(R.id.tvBrand)
+            val tvSerialNumber: TextView = itemView.findViewById(R.id.tvSerialNumber)
+            val tvStatus: TextView = itemView.findViewById(R.id.tvStatus)
+            val ivEdit: ImageView = itemView.findViewById(R.id.ivEdit)
         }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VehicleViewHolder {
+            val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.vehicle_card, parent, false)
+
+            return VehicleViewHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: VehicleViewHolder, position: Int) {
+            val vehicle = vehicles[position]
+
+            holder.ivVehicleImage.setImageResource(vehicle.imageRes)
+            holder.tvVehicleName.text = vehicle.name
+            holder.tvBrand.text = "Marca: ${vehicle.brand}"
+            holder.tvSerialNumber.text = "No. Serie: ${vehicle.serialNumber}"
+            holder.tvStatus.text = vehicle.status
+            holder.tvStatus.setTextColor(ContextCompat.getColor(holder.itemView.context, vehicle.statusColor))
+
+            holder.ivEdit.setOnClickListener {
+                Toast.makeText(context, "Editar ${vehicle.name}", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        override fun getItemCount() = vehicles.size
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
+    ): View {
         val view = inflater.inflate(R.layout.fragment_vehicles, container, false)
         view.setBackgroundColor(resources.getColor(R.color.white, null))
-        return view
-    }
+        val vehicles = listOf(
+            Vehicle(
+                R.drawable.chevy_pop,
+                "Chevy Pop 2002",
+                "Chevrolet",
+                "FR08104",
+                "En registro",
+                R.color.green
+            ),
+            Vehicle(
+                R.drawable.tsuru_tuneado,
+                "Tsuru tunning",
+                "Nissan",
+                "TS12345",
+                "En taller",
+                R.color.ultraLightBlue
+            ),
+            Vehicle(
+                R.drawable.ford_ka,
+                "Ford Ka",
+                "Ford",
+                "FK67890",
+                "Listo para entregar",
+                R.color.blue
+            ),
+            Vehicle(
+                R.drawable.baseline_directions_car_24,
+                "TEST",
+                "TEST",
+                "FK67890",
+                "Listo para entregar",
+                R.color.blue
+            ),
+            Vehicle(
+                R.drawable.baseline_directions_car_24,
+                "TEST",
+                "TEST",
+                "FK67890",
+                "Listo para entregar",
+                R.color.blue
+            ),
+            Vehicle(
+                R.drawable.baseline_directions_car_24,
+                "TEST",
+                "TEST",
+                "FK67890",
+                "Listo para entregar",
+                R.color.blue
+            ),
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment VehiclesFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            VehiclesFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+
+        )
+
+        val recyclerView = view.findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        recyclerView.adapter = VehicleAdapter(vehicles)
+        recyclerView.isNestedScrollingEnabled = true
+        recyclerView.addItemDecoration(
+            DividerItemDecoration(context, DividerItemDecoration.VERTICAL).apply {
+                setDrawable(ContextCompat.getDrawable(requireContext(), R.drawable.list_divider)!!)
             }
+        )
+
+        return view
     }
 }
