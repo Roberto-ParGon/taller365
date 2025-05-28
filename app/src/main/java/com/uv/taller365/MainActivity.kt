36cfa0caa231406a33739f6ee788ff4b5525fe59
Roberto-ParGon <zs19016407@estudiantes.uv.mx>
2025-05-27 18:26:33 -0600

    [ADD]SwipeGestures

--- app/src/main/java/com/uv/taller365/MainActivity.kt
+++ app/src/main/java/com/uv/taller365/MainActivity.kt
@@ -4,41 +4,43 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.core.view.get
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var bottomNav: BottomNavigationView
+    private lateinit var viewPager: ViewPager2
+    private lateinit var adapter: FragmentStateAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        loadFragment(VehiclesFragment())
+
         window.statusBarColor = ContextCompat.getColor(this, R.color.blue)
-        bottomNav = findViewById(R.id.bottomNav)!!
+
+        bottomNav = findViewById(R.id.bottomNav)
+        viewPager = findViewById(R.id.viewpager)
+
+        adapter = FragmentStateAdapter(this)
+        viewPager.adapter = adapter
+
         bottomNav.setOnItemSelectedListener {
             when (it.itemId) {
-                R.id.vehiclesNav -> {
-                    loadFragment(VehiclesFragment())
-                    true
-                }
-                R.id.repaisNav -> {
-                    loadFragment(RepairFragment())
-                    true
-                }
-                R.id.clientsNav -> {
-                    loadFragment(ClientsFragment())
-                    true
-                }
-                else -> false
+                R.id.vehiclesNav -> viewPager.currentItem = 0
+                R.id.repaisNav -> viewPager.currentItem = 1
+                R.id.clientsNav -> viewPager.currentItem = 2
             }
+            true
         }
-    }
 
-    private fun loadFragment(fragment: Fragment) {
-        val transaction = supportFragmentManager.beginTransaction()
-        transaction.replace(R.id.container, fragment)
-        transaction.commit()
+        viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                bottomNav.menu[position].isChecked = true
+            }
+        })
     }
-
 }
+
